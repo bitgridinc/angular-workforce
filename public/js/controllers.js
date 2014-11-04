c5f371933282e92271dad432d41d17b7ca048bc9
Chris Hairfield <chairfield@gmail.com>
2014-11-04 14:52:39 -0800

    Introduced the submit logic. Currently, it redirects the user to the /respond page.

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,9 +1,13 @@
 "use strict";
 
 angular.module('app.controllers', []).
-  controller('requestController', function ($scope) {
+  controller('requestController', function ($scope, $location) {
     $scope.form = {};
     $scope.form.org = "BitGrid";
     $scope.form.lat = "1";
     $scope.form.lon = "2";
+
+    $scope.form.submitForm = function(item, event) {
+      $location.path('/respond');
+    };
   });
