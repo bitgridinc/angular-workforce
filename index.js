a3ca8132d18aefa4d7cdcbe0ae8a1ce7ce310ed2
Chris Hairfield <chairfield@gmail.com>
2014-11-10 11:10:46 -0800

    I've finalized on app as the root name as I intend for the app folder to contain the entire front end application. This includes bower_components, so I've moved them inside.

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ var server = new hapi.Server('localhost', PORT);
 
 // routes
 server.route([        
-  { method: 'GET', path: '/partials/{path*}', handler: { directory: { path: './public/views/partials' } } }, 
-  { method: 'GET', path: '/js/{path*}', handler: { directory: { path: './public/js' } } },
-  { method: 'GET', path: '/vendor/{path*}', handler: { directory: { path: './public/vendor' } } },
-  { method: 'GET', path: '/bower/{path*}', handler: { directory: { path: './bower_components' } } },
+  { method: 'GET', path: '/partials/{path*}', handler: { directory: { path: './app/views/partials' } } }, 
+  { method: 'GET', path: '/js/{path*}', handler: { directory: { path: './app/js' } } },
+  { method: 'GET', path: '/vendor/{path*}', handler: { directory: { path: './app/vendor' } } },
+  { method: 'GET', path: '/bower/{path*}', handler: { directory: { path: './app/bower_components' } } },
   // Entry point to Angular application
-  { method: 'GET', path: '/{path*}', handler: {file: './public/views/index.html'} },
+  { method: 'GET', path: '/{path*}', handler: {file: './app/views/index.html'} },
   // Entry point to Mocha tests
-  { method: 'GET', path: '/tests/{path*}', handler: {file: './public/views/tests.html'} }
+  { method: 'GET', path: '/tests/{path*}', handler: {file: './app/views/tests.html'} }
 ]);
 
 server.start();
