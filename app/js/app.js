75675ec6e7833d293eb906d4299bf66c35e2d070
Chris Hairfield <chairfield@gmail.com>
2014-11-10 21:21:20 -0800

    We have a map! I'm using the angular-leaflet-directive module to display and empty map on the /map endpoint.

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,13 @@
 "use strict";
 
 require('./controllers.js');
+require('../bower_components/leaflet');
+require('../bower_components/angular-leaflet-directive/dist/angular-leaflet-directive.js');
 
-var app = angular.module('app', ['ngRoute', 'app.controllers']).
+var app = angular.module('app', ['ngRoute', 'app.controllers', 'leaflet-directive']).
   config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider
+      .when('/map', {templateUrl: 'partials/map.html'})
       .when('/request', {templateUrl: 'partials/request.html', controller: 'requestController'})
       .when('/respond', {templateUrl: 'partials/respond.html', controller: 'respondController'})
       .otherwise({redirectTo: '/request'});
