8bc8334687828f6632ad245798fa2bfc7e4dc602
Chris Hairfield <chairfield@gmail.com>
2014-11-11 16:45:57 -0800

    I'm drawing markers on the map when it's clicked.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -17,14 +17,30 @@ angular.module('app.controllers', [])
       },
       controls: {
         draw: {}
-      }
+      },
+      events: {}
     });
+
+    // Simply to figure out how to add controls, here's the draw control
     leafletData.getMap().then(function(map) {
       var drawnItems = $scope.controls.draw.edit.featureGroup;
       map.on('draw:created', function (e) {
         var layer = e.layer;
         drawnItems.addLayer(layer);
         console.log(JSON.stringify(layer.toGeoJSON()));
+      });
+    });
+
+    // Place markers when the user clicks on the map
+    $scope.markers = new Array();
+
+    $scope.$on("leafletDirectiveMap.click", function(event, args){
+      var leafEvent = args.leafletEvent;
+
+      $scope.markers.push({
+        lat: leafEvent.latlng.lat,
+        lng: leafEvent.latlng.lng,
+        message: "My Added Marker"
       });
     });
   })
