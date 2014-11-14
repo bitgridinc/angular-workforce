4c1db49b2dd10767eaaa8fc779e2988c98cdbd94
Chris Hairfield <chairfield@Chriss-MacBook-Pro.local>
2014-11-14 11:55:40 -0500

    We only have one path for the map, as the request and response UIs are opened in dialogs.

--- app/js/app.js
+++ app/js/app.js
@@ -5,16 +5,12 @@ require('./controllers.js');
 var app = angular.module('app', ['ngRoute', 'app.controllers', 'leaflet-directive']).
 config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
   $routeProvider
-    .when('/map', {
+    .when('/', {
       templateUrl: 'partials/map.html',
       controller: 'mapController'
     })
-    .when('/respond', {
-      templateUrl: 'partials/respond.html',
-      controller: 'respondController'
-    })
     .otherwise({
-      redirectTo: '/request'
+      redirectTo: '/'
     });
   $locationProvider.html5Mode(true);
 }]);
