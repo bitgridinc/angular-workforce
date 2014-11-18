39b43821c8ec8150d7a5eee7e6d140c4656950e5
Chris Hairfield <chairfield@gmail.com>
2014-11-17 18:43:58 -0800

    Improving formatting.

--- client/app/app.js
+++ client/app/app.js
@@ -3,8 +3,12 @@
 require('../bower_components/angular/angular.js');
 require('./homePage/homePage.js');
 
-var app = angular.module('app', ['ngRoute', 'app.homePage', 'leaflet-directive']).
-config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+var app = angular.module('app', [
+  'ngRoute',
+  'app.homePage',
+  'leaflet-directive']);
+
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
   $routeProvider
     .when('/', {
       templateUrl: 'partials/homePage.html',
