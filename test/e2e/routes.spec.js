f99c116139404a88def9c127a9c479faf9e81d11
Chris Hairfield <chairfield@gmail.com>
2014-11-20 08:21:17 -0800

    Removing #'s I didn't understand.

--- test/e2e/routes.spec.js
+++ test/e2e/routes.spec.js
@@ -2,10 +2,10 @@
 
 describe('routes', function() {
   it('should redirect to /dashboard when / is accessed', function() {
-    browser.get('/#/dashboard');
+    browser.get('/dashboard');
     var dashboardUrl = browser.getCurrentUrl();
 
-    browser.get('/#/');
+    browser.get('/');
     expect(browser.getCurrentUrl()).toBe(dashboardUrl);
   });
 });
