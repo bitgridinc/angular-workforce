594f6d6f5220726beace5b296a5f8fc48e1ccd32
Chris Hairfield <chairfield@gmail.com>
2014-11-20 08:05:57 -0800

    Setting better configuration options.

--- protractor.conf.js
+++ protractor.conf.js
@@ -6,5 +6,12 @@ exports.config = {
     './e2e/*.spec.js'
   ],
                      
-  baseUrl: 'http://localhost:8080'
+  baseUrl: 'http://localhost:8080',
+
+  jasmineNodeOpts: {
+    showColors: true,
+    defaultTimeoutInterval: 30000,
+    isVerbose: true,
+    includeStackTrace: true
+  }
 };
