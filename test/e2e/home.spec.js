304477503b5ee66ca3928d14469cfd88929404c8
Chris Hairfield <chairfield@gmail.com>
2014-11-20 08:19:18 -0800

    It's stupid to test our application's title.

--- test/e2e/home.spec.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-describe('home page', function() {
-  it('should set home page title to BitGrid', function() {
-    browser.get('/dashboard');
-    expect(browser.getTitle()).toBe("BitGrid");
-  });
-});
