3e9f1cf2de455ad677351cebdffac6c90fc00f05
Chris Hairfield <chairfield@gmail.com>
2014-11-19 20:10:32 -0800

    I like this indentation scheme better.

--- client/app/dashboard/dashboard.js
+++ client/app/dashboard/dashboard.js
@@ -9,102 +9,102 @@ require('../../common/services/beaconService.js');
 
 angular.module('dashboard', ['ngDialog', 'ui.bootstrap', 'app.services'])
 
-  .config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/dashboard', {
-      templateUrl: 'partials/dashboard.html',
-      controller: 'DashboardCtrl'
-    });
-  }])
-
-  .controller('DashboardCtrl', function($scope, leafletData, ngDialog) {
-    angular.extend($scope, {
-      defaults: {
-        tileLayer: "http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png",
-        zoomControl: false
-      },
-      washdc: {
-        lat: 38.914268,
-        lng: -77.021098,
-        zoom: 13
-      },
-      events: {},
-      markers: []
-    });
-
-    $scope.status = {
-      showMidColumn: true,
-      showRightColumn: true
-    };
-
-    // I'm using this to easily populate the latitude and longitude fields on Create Beacon
-    $scope.$on("leafletDirectiveMap.click", function(clickEvent, clickArgs) {
-      console.log('leafletDirectiveMap.click');
-      $scope.lastlatlng = clickArgs.leafletEvent.latlng;
-    });
-
-    // clickArgs will contain the marker name and other relevant information
-    $scope.$on('leafletDirectiveMarker.click', function(clickEvent, clickArgs) {
-      console.log('Opening dialog to respond to clicked beacon.', clickArgs.leafletEvent.target);
-      var lastSelectedMarker = clickArgs.leafletEvent.target;
-
-      ngDialog.openConfirm({
-        template: '/partials/respond.html',
-        className: 'ngdialog-theme-default',
-        controller: 'respondController',
-        data: clickArgs.leafletEvent.target.options
-      }).then(function(ngDialogData) {
-        console.log('Modal response dialog promise resolved. Value: ', ngDialogData);
-        for (var i = 0; i < $scope.markers.length; i++) {
-          if ($scope.markers[i].$$hashKey === lastSelectedMarker.options.$$hashKey) {
-            $scope.markers[i].responderName = ngDialogData.responderName;
-            $scope.markers[i].numResponders = ngDialogData.numResponders;
-          }
-        }
-      }, function(reason) {
-        console.log('Modal response dialog promise rejected. Reason: ', reason);
-      });
-    });
-  })
-
-  .controller('requestController', function($scope) {
-    angular.extend($scope, {
-      newBeaconData: {
-        title: 'Job Title',
-        description: 'Project Description',
-        organization: 'Organization'
-      },
-      submitNewBeacon: function() {
-        console.log("submitNewBeacon called.");
-        $scope.markers.push({
-          title: $scope.newBeaconData.title,
-          description: $scope.newBeaconData.description,
-          organization: $scope.newBeaconData.organization,
-          lat: $scope.lastlatlng.lat,
-          lng: $scope.lastlatlng.lng
-        });
-        $scope.status.showRightColumn = false;
-        alert('You have successfully created a new beacon.');
-      },
-      deleteNewBeacon: function() {
-        console.log("deleteNewBeacon called.");
-        $scope.status.showRightColumn = false;
-      }
-    });
-  })
-
-  .controller('respondController', function($scope) {
-    $scope.assistForm = {};
-    $scope.assistForm.numResponders = 2;
-
-    $scope.assistForm.offerAssistance = function() {
-      alert("You've accepted!");
-      console.log("You've accepted! $scope:", $scope);
-      angular.extend($scope.ngDialogData, {
-        responderName: $scope.assistForm.responderName,
-        numResponders: $scope.assistForm.numResponders
-      });
-    };
-    $scope.assistForm.declineAssistance = function() {
-      alert("You've declined!");
-    };
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/dashboard', {
+    templateUrl: 'partials/dashboard.html',
+    controller: 'DashboardCtrl'
   });
+}])
+
+.controller('DashboardCtrl', function($scope, leafletData, ngDialog) {
+  angular.extend($scope, {
+    defaults: {
+      tileLayer: "http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png",
+      zoomControl: false
+    },
+    washdc: {
+      lat: 38.914268,
+      lng: -77.021098,
+      zoom: 13
+    },
+    events: {},
+    markers: []
+  });
+
+  $scope.status = {
+    showMidColumn: true,
+    showRightColumn: true
+  };
+
+  // I'm using this to easily populate the latitude and longitude fields on Create Beacon
+  $scope.$on("leafletDirectiveMap.click", function(clickEvent, clickArgs) {
+    console.log('leafletDirectiveMap.click');
+    $scope.lastlatlng = clickArgs.leafletEvent.latlng;
+  });
+
+  // clickArgs will contain the marker name and other relevant information
+  $scope.$on('leafletDirectiveMarker.click', function(clickEvent, clickArgs) {
+    console.log('Opening dialog to respond to clicked beacon.', clickArgs.leafletEvent.target);
+    var lastSelectedMarker = clickArgs.leafletEvent.target;
+
+    ngDialog.openConfirm({
+      template: '/partials/respond.html',
+      className: 'ngdialog-theme-default',
+      controller: 'respondController',
+      data: clickArgs.leafletEvent.target.options
+    }).then(function(ngDialogData) {
+      console.log('Modal response dialog promise resolved. Value: ', ngDialogData);
+      for (var i = 0; i < $scope.markers.length; i++) {
+        if ($scope.markers[i].$$hashKey === lastSelectedMarker.options.$$hashKey) {
+          $scope.markers[i].responderName = ngDialogData.responderName;
+          $scope.markers[i].numResponders = ngDialogData.numResponders;
+        }
+      }
+    }, function(reason) {
+      console.log('Modal response dialog promise rejected. Reason: ', reason);
+    });
+  });
+})
+
+.controller('requestController', function($scope) {
+  angular.extend($scope, {
+    newBeaconData: {
+      title: 'Job Title',
+      description: 'Project Description',
+      organization: 'Organization'
+    },
+    submitNewBeacon: function() {
+      console.log("submitNewBeacon called.");
+      $scope.markers.push({
+        title: $scope.newBeaconData.title,
+        description: $scope.newBeaconData.description,
+        organization: $scope.newBeaconData.organization,
+        lat: $scope.lastlatlng.lat,
+        lng: $scope.lastlatlng.lng
+      });
+      $scope.status.showRightColumn = false;
+      alert('You have successfully created a new beacon.');
+    },
+    deleteNewBeacon: function() {
+      console.log("deleteNewBeacon called.");
+      $scope.status.showRightColumn = false;
+    }
+  });
+})
+
+.controller('respondController', function($scope) {
+  $scope.assistForm = {};
+  $scope.assistForm.numResponders = 2;
+
+  $scope.assistForm.offerAssistance = function() {
+    alert("You've accepted!");
+    console.log("You've accepted! $scope:", $scope);
+    angular.extend($scope.ngDialogData, {
+      responderName: $scope.assistForm.responderName,
+      numResponders: $scope.assistForm.numResponders
+    });
+  };
+  $scope.assistForm.declineAssistance = function() {
+    alert("You've declined!");
+  };
+});
