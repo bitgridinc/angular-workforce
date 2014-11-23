69d32bcbb3ba88efd05edd7915669e7962410cf7
Chris Hairfield <chairfield@gmail.com>
2014-11-23 08:12:29 -0800

    Improving the wording of the beaconService tests.

--- test/unit/beaconService.spec.js
+++ test/unit/beaconService.spec.js
@@ -9,18 +9,18 @@ describe('beaconService', function() {
     beaconService = _BeaconService_;
   }));
 
-  it('should be defined as BeaconService', function() {
+  it('should be defined in Angular', function() {
+    expect(beaconService).toBeDefined();
+  });
+  it('should define an array of beacons', function() {
     expect(beaconService.markers).toBeDefined();
   });
-  it('should define a markers array', function() {
-    expect(beaconService.markers).toBeDefined();
-  });
-  it('should define a createBeacon method', function() {
+  it('should define a method to create a new beacon', function() {
     expect(beaconService.createBeacon).toBeDefined();
   });
 
   describe('createBeacon', function() {
-    it('should add beaconData to markers', function() {
+    it('should add created beacons to the array of beacons', function() {
       var beaconData = {};
       expect(beaconService.markers.length).toBe(0);
       beaconService.createBeacon(beaconData);
