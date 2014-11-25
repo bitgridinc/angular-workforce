f7feba053240b2e90b5ff9b4ce132a4f052f9822
Chris Hairfield <chairfield@gmail.com>
2014-11-25 08:23:11 -0800

    Moved unit tests into usefully-named folders.

--- karma.conf.js
+++ karma.conf.js
@@ -9,7 +9,7 @@ module.exports = function (config) {
       'server/public/js/bundle.js',
       'node_modules/angular-mocks/*.js',
       'test/integration/*.spec.js',
-      'test/unit/*.spec.js'
+      'test/unit/**/*.spec.js'
     ],
                                              
     reporters: ['story'],
