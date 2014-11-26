fff4b70fd0fcf44a293a502eb2ab086d520c6df5
Chris Hairfield <chairfield@gmail.com>
2014-11-26 13:18:01 -0800

    Implementing an id property on beacons.

--- client/common/services/beaconService.js
+++ client/common/services/beaconService.js
@@ -3,9 +3,14 @@
 var app = require('./_module_init.js');
 
 app.service('BeaconService', function() {
+  var idCounter = 0;
   return {
     beacons: [],
+    getBeacon: function() {
+      return undefined;
+    },
     createBeacon: function(beaconData) {
+      beaconData.id = idCounter++;
       this.beacons.push(beaconData);
     }
   }
--- test/unit/services/beaconService.spec.js
+++ test/unit/services/beaconService.spec.js
@@ -15,17 +15,35 @@ describe('beaconService', function() {
   it('should define an array of beacons', function() {
     expect(beaconService.beacons).toBeDefined();
   });
-  it('should define a method to create a new beacon', function() {
-    expect(beaconService.createBeacon).toBeDefined();
+
+  describe('the method to get a beacon by id', function() {
+    it('should be defined', function() {
+      expect(beaconService.getBeacon).toBeDefined();
+    });
+    it('should return undefined when this service is instantiated', function() {
+      expect(beaconService.getBeacon(0)).toBeUndefined();
+    });
   });
 
-  describe('createBeacon', function() {
+  describe('the method used to create a beacon', function() {
+    it('should be defined', function() {
+      expect(beaconService.createBeacon).toBeDefined();
+    });
     it('should add created beacons to the array of beacons', function() {
       var beaconData = {};
       expect(beaconService.beacons.length).toBe(0);
       beaconService.createBeacon(beaconData);
       expect(beaconService.beacons.length).toBe(1);
       expect(beaconService.beacons[0]).toBe(beaconData);
+    });
+    it('should create an id property with a positive integer value on the created beacon', function() {
+      beaconService.createBeacon({});
+      expect(beaconService.beacons[0].id).toBeGreaterThan(-1);
+    });
+    it('should create a unique id property on subsequent creates', function() {
+      beaconService.createBeacon({});
+      beaconService.createBeacon({});
+      expect(beaconService.beacons[0].id).not.toBe(beaconService.beacons[1].id);
     });
   });
 });
