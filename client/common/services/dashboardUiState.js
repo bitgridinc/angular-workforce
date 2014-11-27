21d27bca3d7410992a293b6c4ad24c11f4cd2202
Chris Hairfield <chairfield@gmail.com>
2014-11-26 20:03:17 -0800

    Adding a comment to myself.

--- client/common/services/dashboardUiState.js
+++ client/common/services/dashboardUiState.js
@@ -13,6 +13,7 @@ app.service('DashboardUiState', function() {
       return currentlySelectedBeacon;
     },
     // The Tell-Don't-Ask principle says that toggling logic must reside within this service.
+    // Note: We want to toggle here because the wire-frame states the summary must be clicked to close the details page.
     toggleBeaconSelection: function(possiblySelectedBeacon) {
       console.log("toggleBeaconSelection called with:", possiblySelectedBeacon);
       if (angular.isUndefined(currentlySelectedBeacon)) {
