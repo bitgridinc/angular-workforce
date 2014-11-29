25859e79ca67f9402730f1c68c84647b8eae6a6b
Chris Hairfield <chairfield@gmail.com>
2014-11-29 08:29:19 -0800

    Trivial formatting.

--- test/integration/modules/dashboardModule.spec.js
+++ test/integration/modules/dashboardModule.spec.js
@@ -19,26 +19,26 @@ describe('the module managing the dashboard', function() {
       deps = module.value('modules.dashboard').requires;
     });
 
-    it ('should include our Leaflet module', function() {
+    it('should include our Leaflet module', function() {
       expect(hasModule('modules.leaflet')).toEqual(true);
     });
-    it ('should include our cascading collapse module', function() {
+    it('should include our cascading collapse module', function() {
       expect(hasModule('modules.cascadingCollapse')).toEqual(true);
     });
-    it ('should include the Angular routing service', function() {
+    it('should include the Angular routing service', function() {
       expect(hasModule('ngRoute')).toEqual(true);
     });
-    it ('should include the Angular bootstrap module', function() {
+    it('should include the Angular bootstrap module', function() {
       expect(hasModule('ui.bootstrap')).toEqual(true);
     });
 
-    it ('should not include our directives module because the app module does', function() {
+    it('should not include our directives module because the app module does', function() {
       expect(hasModule('modules.directives')).not.toEqual(true);
     });
-    it ('should not include our services module because the app module does', function() {
+    it('should not include our services module because the app module does', function() {
       expect(hasModule('modules.services')).not.toEqual(true);
     });
-    it ('should not include Leaflet directives; they should be wrapped by the Leaflet module', function() {
+    it('should not include Leaflet directives; they should be wrapped by the Leaflet module', function() {
       expect(hasModule('leaflet-directive')).not.toEqual(true);
     });
   })
