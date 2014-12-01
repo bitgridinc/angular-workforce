b700dc17bb82fe066ebb7e036a8dc63961048d13
Chris Hairfield <chairfield@gmail.com>
2014-12-01 09:16:01 -0800

    Using an excellent validator to ensure that only files ending with .tpl.html are exposed.

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 var hapi = require('hapi');
+var templateValidator = require('joi').string().regex(/\.tpl\.html$/, 'template');
 
 // Constants
 var PORT = 8080;
@@ -8,12 +9,19 @@ var server = new hapi.Server('0.0.0.0', PORT);
 
 // routes
 server.route([
-  // Expose the contents of the compiled public files (e.g., bundle.js)
-  { method: 'GET', path: '/js/{path*}', handler: { directory: { path: './server/public/js' } } },
+  // Expose the public folder
+  { method: 'GET', path: '/js/bundle.js', handler: { file: './server/public/js/bundle.js' } },
   { method: 'GET', path: '/stylesheets/{path*}', handler: { directory: { path: './server/public/stylesheets' } } },
 
-  { method: 'GET', path: '/templates/dashboard/{path*}', handler: { directory: { path: './client/app/dashboard' } } },
-  { method: 'GET', path: '/templates/leaflet/{path*}', handler: { directory: { path: './client/app/leaflet' } } },
+  // Expose our templates
+  { method: 'GET', path: '/templates/dashboard/{file}',
+    handler: { directory: { path: './client/app/dashboard' } },
+    config: { validate: { params: { file: templateValidator } } }
+  },
+  { method: 'GET', path: '/templates/leaflet/{file}',
+    handler: { directory: { path: './client/app/leaflet' } },
+    config: { validate: { params: { file: templateValidator } } }
+  },
 
   //{ method: 'GET', path: '/templates/{path*}', handler: { directory: { path: './client/views/templates' } } },
   { method: 'GET', path: '/templates/directives/{path*}', handler: { directory: { path: './client/views/templates/directives' } } },
