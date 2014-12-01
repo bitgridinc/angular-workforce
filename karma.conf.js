0f247a02be1ba53a3f0effb30c603a3f07765cc6
Chris Hairfield <chairfield@gmail.com>
2014-12-01 08:12:50 -0800

    Fixing spacing in karma conf.

--- karma.conf.js
+++ karma.conf.js
@@ -2,18 +2,18 @@ module.exports = function (config) {
   'use strict';
   config.set({
     basePath: '',
-     
+
     frameworks: ['jasmine'],
-     
+
     files: [
       'server/public/js/bundle.js',
       'node_modules/angular-mocks/*.js',
       'test/integration/**/*.spec.js',
       'test/unit/**/*.spec.js'
     ],
-                                             
+
     reporters: ['story'],
-                                                     
+
     port: 9876,
     colors: true,
     autoWatch: true,
@@ -22,7 +22,7 @@ module.exports = function (config) {
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
-
+    
     browsers: ['PhantomJS'] 
   });
 };
