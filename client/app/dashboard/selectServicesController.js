b658d26e1bd7e7fb3fb522b2367d42fa7bf04dcc
Chris Hairfield <chairfield@gmail.com>
2014-12-01 12:32:03 -0800

    We're displaying the responder info summary on the select services view.

--- client/app/dashboard/selectServicesController.js
+++ client/app/dashboard/selectServicesController.js
@@ -2,5 +2,6 @@
 
 var app = require('./_module_init.js');
 
-app.controller('SelectServicesController', function($scope, BeaconService) {
+app.controller('SelectServicesController', function($scope, DashboardUiState) {
+  $scope.dashboardUiState = DashboardUiState;
 });
