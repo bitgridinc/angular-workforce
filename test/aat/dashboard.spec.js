4c8b45fb5929e7a46dfc3b532d3a4b083815d92c
Chris Hairfield <chairfield@gmail.com>
2014-12-05 13:01:42 -0800

    Moving an AAT to a better place.

--- test/aat/dashboard.spec.js
+++ test/aat/dashboard.spec.js
@@ -14,10 +14,11 @@ describe('the dashboard page', function() {
   });
   it('should have a button that says My\\nCompany', function() {
     expect(ptor.isElementPresent(by.buttonText('My\nCompany'))).toBeTruthy();
-  });*/
+  });
   it('should have a button that says My\\nCompany', function() {
     expect(element(by.buttonText('My\nCompany')).isDisplayed()).toBeTruthy();
-  });
+  });*/
+
   // This is hackish since I couldn't get the line By.tagName('leaflet') to work...
   it('should have a leaflet element', function() {
     ptor.findElement(protractor.By.id('leaflet'));
@@ -25,4 +26,11 @@ describe('the dashboard page', function() {
   it('should not be displaying the user\'s beacon summary list', function() {
     expect(element(by.buttonText('Create Beacon')).isDisplayed()).toBeFalsy();
   });
+
+  describe('the My Company button', function() {
+    it('should change the url when clicked', function() {
+      ptor.findElement(protractor.By.buttonText('My\nCompany')).click();
+      expect(browser.getCurrentUrl()).toContain('/#/dashboard/mycompany');
+    });
+  })
 });
