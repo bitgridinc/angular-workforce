659dd272d7f117f7f1bf5c2b9c1db373c9bc187c
Chris Hairfield <chairfield@gmail.com>
2014-12-08 14:27:12 -0800

    Adding a test that won't work.

--- test/aat/offerAssistance.spec.js
+++ test/aat/offerAssistance.spec.js
@@ -15,6 +15,11 @@ describe('the beacon details view', function() {
     offerAssistanceLocators = new OfferAssistanceLocators();
   });
 
+  // TODO: I'd like some way to ensure that there is a defined beacon in scope
+  /*it('should display the name of the company you\'re offering to help', function() {
+    expect(element(by.text('has requested')).isPresent()).toBeTruthy();
+  });*/
+
   describe('the assist button', function() {
     it('should change the url when clicked', function() {
       ptor.findElement(offerAssistanceLocators.assistButton).click();
