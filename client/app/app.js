09efb88bd374944bff0026360df93ce1554e644f
Chris Hairfield <chairfield@gmail.com>
2014-12-16 12:27:25 -0800

    I've learned about HTML5 mode and updated my comment.

--- client/app/app.js
+++ client/app/app.js
@@ -37,7 +37,11 @@ app.config(
     function($stateProvider, $urlRouterProvider, $locationProvider) {
       $urlRouterProvider.otherwise('/dashboard');
 
-      // TODO: This prevents my from directly accessing child pages (e.g., /dashboard/mycompany) and I don't know why
+      // Normally, the '#' is used to implement routing in the url of a single-page application as changes in the
+      // comment don't trigger a page load. HTML5 mode allows for this same routing but native in the browser. There are
+      // two problems, though. First, browser support for this isn't consistent. Second, we would need to update the
+      // server to serve our application on every possible route within the application (because a user pasting in a
+      // copied url shouldn't get a 404). The downside is that we have a '#' in our urls. Big whoop.
       //$locationProvider.html5Mode(true);
     }
   ]
