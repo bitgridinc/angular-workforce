a5e46761fa617b8778efa49998eef8e4cae51e4d
Chris Hairfield <chairfield@gmail.com>
2014-12-18 09:47:07 -0800

    Here's a verbose sanity check to run by Sahil.

--- client/common/directives/organizationLabel.js
+++ client/common/directives/organizationLabel.js
@@ -12,8 +12,12 @@ require('./_module_init.js')
         controller: function($scope) {
           if (angular.isUndefined($scope.organization)) {
             console.log('organization is undefined and it never should be');
+          } else if (!angular.isObject($scope.organization)) {
+            console.log('organization isn\'t an object and it must be.');
           } else if (angular.isUndefined($scope.organization.name)) {
             console.log('organization.name is undefined and it never should be');
+          } else if (!angular.isString($scope.organization.name)) {
+            console.log('organization.name isn\'t a string is and must be');
           }
         }
       }
