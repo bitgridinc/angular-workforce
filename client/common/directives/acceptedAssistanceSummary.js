819e30534c1ed3f665e4007e87a87826a662b62d
Chris Hairfield <chairfield@gmail.com>
2014-12-18 09:50:12 -0800

    Started to add logs for another directive.

--- client/common/directives/acceptedAssistanceSummary.js
+++ client/common/directives/acceptedAssistanceSummary.js
@@ -12,7 +12,16 @@ require('./_module_init.js')
                     '{{acceptedAssistance.responderName}} is coming with ' +
                     '{{acceptedAssistance.numResponders}} people at ' +
                     '{{acceptedAssistance.arrivalDate}}.' +
-                  '</div>'
+                  '</div>',
+        controller: function($scope) {
+          if (angular.isUndefined($scope.acceptedAssistance)) {
+            console.log('acceptedAssistance is undefined and it never should be');
+          } else if (!angular.isObject($scope.acceptedAssistance)) {
+            console.log('acceptedAssistance isn\'t an object and it must be.');
+          }
+          // TODO: We'd like the functionality to log if the object isn't how we expect, but I don't like checking
+          // TODO: against every property here. Consider alternatives.
+        }
       }
     }
   );
