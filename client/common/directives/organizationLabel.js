99b33a53e0de0a7d9487f13fe201d19d600502ec
Chris Hairfield <chairfield@gmail.com>
2014-12-18 09:33:04 -0800

    Adding sanity checks to a directive.

--- client/common/directives/organizationLabel.js
+++ client/common/directives/organizationLabel.js
@@ -8,7 +8,14 @@ require('./_module_init.js')
         scope: {
           organization: '='
         },
-        template: '{{organization.name}}'
+        template: '{{organization.name}}',
+        controller: function($scope) {
+          if (angular.isUndefined($scope.organization)) {
+            console.log('organization is undefined and it never should be');
+          } else if (angular.isUndefined($scope.organization.name)) {
+            console.log('organization.name is undefined and it never should be');
+          }
+        }
       }
     }
   );
