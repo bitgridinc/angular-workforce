171d1e5f2230f07bc753dd7ceedc7a35cb7e0609
Chris Hairfield <chairfield@gmail.com>
2014-12-18 09:12:31 -0800

    Formatted my directives as I do my modules.

--- client/common/directives/acceptedAssistanceSummary.js
+++ client/common/directives/acceptedAssistanceSummary.js
@@ -1,17 +1,18 @@
 "use strict";
 
-var app = require('./_module_init.js');
-
-app.directive('acceptedAssistanceSummary', [function() {
-  return {
-    restrict: 'E',
-    scope: {
-      acceptedAssistance: '='
-    },
-    template: '<div class="panel panel-info">' +
-                '{{acceptedAssistance.responderName}} is coming with ' +
-                '{{acceptedAssistance.numResponders}} people at ' +
-                '{{acceptedAssistance.arrivalDate}}.' +
-              '</div>'
-  }
-}]);
+require('./_module_init.js')
+  .directive('acceptedAssistanceSummary',
+    function() {
+      return {
+        restrict: 'E',
+        scope: {
+          acceptedAssistance: '='
+        },
+        template: '<div class="panel panel-info">' +
+                    '{{acceptedAssistance.responderName}} is coming with ' +
+                    '{{acceptedAssistance.numResponders}} people at ' +
+                    '{{acceptedAssistance.arrivalDate}}.' +
+                  '</div>'
+      }
+    }
+  );
--- client/common/directives/beaconSummary.js
+++ client/common/directives/beaconSummary.js
@@ -1,16 +1,17 @@
 "use strict";
 
-var app = require('./_module_init.js');
-
-app.directive('beaconSummary', [function() {
-  return {
-    // Restricted to only look for this directive in elements
-    restrict: 'E',
-    templateUrl: '/templates/directives/beaconSummary.tpl.html',
-    scope: {
-      beacon: '=',
-      onSelectBeacon: '=',
-      onReviewAssistance: '='
+require('./_module_init.js')
+  .directive('beaconSummary',
+    function() {
+      return {
+        // Restricted to only look for this directive in elements
+        restrict: 'E',
+        templateUrl: '/templates/directives/beaconSummary.tpl.html',
+        scope: {
+          beacon: '=',
+          onSelectBeacon: '=',
+          onReviewAssistance: '='
+        }
+      }
     }
-  }
-}]);
+  );
