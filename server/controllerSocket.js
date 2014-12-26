eed8d31b2b4bd81cf60861ae91e2c237cf21ae90
Chris Hairfield <chairfield@gmail.com>
2014-12-26 11:07:24 -0800

    Replaced the guid method from remote retro with the uuid module.

--- server/controllerSocket.js
+++ server/controllerSocket.js
@@ -2,16 +2,16 @@
 'use strict';
 
 var io = require('./socketSetup').instance;
+var uuid = require('node-uuid');
 var generator = require('./generator');
 
 io.sockets.on('connection', function(socket){
-
   socket.emit('init', {
     name: generator.generateName()
   });
 
   socket.on('send:request', function(request) {
-    request.id = generator.generateGuid();
+    request.id = uuid.v4();
 
     // This sends back to the sender as well
     io.sockets.emit('send:request', request);
--- server/helpers.js
+++ server/helpers.js
@@ -1,14 +1,6 @@
 "use strict";
 
 module.exports = {
-  guid: function () {
-    function _p8(s) {
-      var p = (Math.random().toString(16) + "000000000").substr(2, 8);
-      return s ? "-" + p.substr(0, 4) + "-" + p.substr(4, 4) : p;
-    }
-
-    return _p8() + _p8(true) + _p8(true) + _p8();
-  },
   toTitleCase: function(str){
     /*
      * To Title Case 2.1 – http://individed.com/code/to-title-case/
