ab6fde3e9e2785650edf9dfb6b4e14c01c072196
Chris Hairfield <chairfield@gmail.com>
2014-12-31 14:11:48 -0800

    Adding what was a failing AAT to ensure that we only display the offer assistance view if the beacon exists. All AATs are passing now.

---
+++ test/aat/offerAssistance.nonExistent.spec.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var Locators = require('./offerAssistance.locators.js');
+
+describe('trying to assist a non-existent beacon', function() {
+  var ptor,
+      locators;
+
+  beforeEach(function() {
+    ptor = protractor.getInstance();
+    ptor.get('/#/dashboard/mycompany/detail/99999non-exis-tent-9999-999999999999/assist');
+  });
+
+  beforeEach(function() {
+    locators = new Locators();
+  });
+
+  it('should display an error page', function() {
+    expect(browser.getCurrentUrl()).toContain('/#/dashboard/mycompany/detail/99999non-exis-tent-9999-999999999999/assist');
+    expect(element(locators.assistButton).isDisplayed()).toBeFalsy();
+    expect(element(locators.declineButton).isDisplayed()).toBeFalsy();
+  });
+});
