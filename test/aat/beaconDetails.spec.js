d5733c50d76c0fe2b90e06b27ee3adb979989420
Chris Hairfield <chairfield@gmail.com>
2014-12-31 08:20:00 -0800

    Improving my tests a bit.

--- test/aat/beaconDetails.spec.js
+++ test/aat/beaconDetails.spec.js
@@ -15,12 +15,15 @@ describe('the beacon details view', function() {
     beaconDetailsLocators = new BeaconDetailsLocators();
   });
 
-  it('should navigate to the details url', function() {
+  it('should navigate to the populated details page', function() {
     expect(browser.getCurrentUrl()).toContain('/#/dashboard/mycompany/detail/e688af0b-63df-48bc-941c-9cc5f750367b');
+    expect(ptor.isElementPresent(beaconDetailsLocators.selectBeacon)).toBeTruthy();
+    expect(ptor.isElementPresent(beaconDetailsLocators.offerAssistance)).toBeTruthy();
+    expect(element(beaconDetailsLocators.goBack).isDisplayed()).toBeFalsy();
   });
 
-  describe('the select beacon button', function() {
-    it('should change the url when clicked', function() {
+  describe('the select beacon summary card', function() {
+    it('should navigate back to the list when clicked', function() {
       ptor.findElement(beaconDetailsLocators.selectBeacon).click();
       expect(browser.getCurrentUrl()).not.toContain('/detail/e688af0b-63df-48bc-941c-9cc5f750367b');
       expect(browser.getCurrentUrl()).toContain('/#/dashboard/mycompany');
