d06d4a2c532c84e713123056b6a1d2677cd399da
Chris Hairfield <chairfield@gmail.com>
2015-01-02 08:41:28 -0800

    Saving the command I use to run nodemon directly.

---
+++ nodemon.sh
@@ -0,0 +1,1 @@
+node_modules/gulp-nodemon/node_modules/nodemon/bin/nodemon.js -e js,html -x server/index.js -w client -w server/public/ -i server/public/js/bundle.js -i .git -i .idea -i node_modules
