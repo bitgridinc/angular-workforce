e0d3a4aeca9ed02c24cb2e3897cdba32a0337d06
Chris Hairfield <chairfield@gmail.com>
2015-01-07 13:52:35 -0800

    Bringing back my integration tests. I'm not sure I want them deleted.

---
+++ client/app/map/integration.spec.js
@@ -0,0 +1,34 @@
+"use strict";
+
+describe('the map module (integration)', function() {
+  var module;
+  beforeEach(function() {
+    module = angular.module('modules.map');
+  });
+
+  it('should be registered with Angular', function() {
+    expect(module).toBeDefined();
+  });
+
+  describe('and its dependencies', function() {
+    var moduleDependencies;
+    var dependencyListHasModule = function(moduleName) {
+      return moduleDependencies.indexOf(moduleName) >= 0;
+    };
+
+    beforeEach(function() {
+      moduleDependencies = module.value('app').requires;
+    });
+
+    beforeEach(function() {
+      moduleDependencies = module.value('modules.map').requires;
+    });
+
+    it ('should only depend on a single module - haha, gotta TDD!', function() {
+      expect(moduleDependencies.length).toEqual(1);
+    });
+    it ('should include Angular directives for Leaflet', function() {
+      expect(dependencyListHasModule('leaflet-directive')).toEqual(true);
+    });
+  })
+});
