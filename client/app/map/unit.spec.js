82debf91f01c34e54f4deae77751acd414776cf2
Chris Hairfield <chairfield@gmail.com>
2015-01-07 13:42:28 -0800

    Combining some trivial unit tests.

--- client/app/map/unit.spec.js
+++ client/app/map/unit.spec.js
@@ -16,29 +16,13 @@ describe("the map module (unit)", function() {
 
   it('should define a defaults object', function() {
     expect(scope.defaults).toBeDefined();
+    expect(scope.defaults.tileLayer).toBeDefined();
+    expect(scope.defaults.zoomControl).toBe(false);
   });
   it('should define a center object', function() {
     expect(scope.center).toBeDefined();
-  });
-
-  describe('the defaults object', function() {
-    it('should define the tileLayer property', function() {
-      expect(scope.defaults.tileLayer).toBeDefined();
-    });
-    it('should disable the zoom control', function() {
-      expect(scope.defaults.zoomControl).toBe(false);
-    });
-  });
-
-  describe('the center object', function() {
-    it('should define a lat property', function() {
-      expect(scope.center.lat).toBeDefined();
-    });
-    it('should define a lng property', function() {
-      expect(scope.center.lng).toBeDefined();
-    });
-    it('should define a zoom property', function() {
-      expect(scope.center.zoom).toBeDefined();
-    });
+    expect(scope.center.lat).toBeDefined();
+    expect(scope.center.lng).toBeDefined();
+    expect(scope.center.zoom).toBeDefined();
   });
 });
