2c899d389e60cafbab83cfe256a11b057c2fa6ee
Chris Hairfield <chairfield@gmail.com>
2015-01-09 09:09:35 -0800

    Trivial.

--- client/app/beaconControl/aat/locators.js
+++ client/app/beaconControl/aat/locators.js
@@ -1,7 +1,7 @@
 "use strict";
 
 var controlLocators = function() {
-  this.myBeaconsButton = by.id('dashboard-beacons-btn')
+  this.myBeaconsButton = by.id('dashboard-beacons-btn');
 };
 
 module.exports = controlLocators;
