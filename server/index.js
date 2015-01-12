0809ac9ae68bd7c776c0259e6dd5d7b5d96fac91
Chris Hairfield <chairfield@gmail.com>
2015-01-12 15:41:36 -0800

    Almost there. Things work. Nodemon is annoying in how it propagates the cwd to dependent ttasks, so I'll put it in a leaf task and see if still does so. The unit tests are also being run twice, but this is a massive improvement over earlier.

--- server/index.js
+++ server/index.js
@@ -15,66 +15,66 @@ server.route([
   { method: 'POST', path: '/beacon', config: controller.createBeacon },*/
 
   // Expose the public folder
-  { method: 'GET', path: '/js/bundle.js', handler: { file: './server/public/js/bundle.js' } },
-  { method: 'GET', path: '/stylesheets/{path*}', handler: { directory: { path: './server/public/stylesheets' } } },
+  { method: 'GET', path: '/js/bundle.js', handler: { file: './public/js/bundle.js' } },
+  { method: 'GET', path: '/stylesheets/{path*}', handler: { directory: { path: './public/stylesheets' } } },
 
   // Expose our feature templates
   { method: 'GET', path: '/templates/_application/{file}',
-    handler: { directory: { path: './client/app/_application' } },
+    handler: { directory: { path: '../client/app/_application' } },
     config: { validate: { params: { file: templateValidator } } }
   },
   { method: 'GET', path: '/templates/beaconControl/{file}',
-    handler: { directory: { path: './client/app/beaconControl' } },
+    handler: { directory: { path: '../client/app/beaconControl' } },
     config: { validate: { params: { file: templateValidator } } }
   },
   { method: 'GET', path: '/templates/beaconDetails/{file}',
-    handler: { directory: { path: './client/app/beaconDetails' } },
+    handler: { directory: { path: '../client/app/beaconDetails' } },
     config: { validate: { params: { file: templateValidator } } }
   },
   { method: 'GET', path: '/templates/createBeacon/{file}',
-    handler: { directory: { path: './client/app/createBeacon' } },
+    handler: { directory: { path: '../client/app/createBeacon' } },
     config: { validate: { params: { file: templateValidator } } }
   },
   { method: 'GET', path: '/templates/dashboard/{file}',
-    handler: { directory: { path: './client/app/dashboard' } },
+    handler: { directory: { path: '../client/app/dashboard' } },
     config: { validate: { params: { file: templateValidator } } }
   },
   { method: 'GET', path: '/templates/header/{file}',
-    handler: { directory: { path: './client/app/header' } },
+    handler: { directory: { path: '../client/app/header' } },
     config: { validate: { params: { file: templateValidator } } }
   },
   { method: 'GET', path: '/templates/listBeacons/{file}',
-    handler: { directory: { path: './client/app/listBeacons' } },
+    handler: { directory: { path: '../client/app/listBeacons' } },
     config: { validate: { params: { file: templateValidator } } }
   },
   { method: 'GET', path: '/templates/map/{file}',
-    handler: { directory: { path: './client/app/map' } },
+    handler: { directory: { path: '../client/app/map' } },
     config: { validate: { params: { file: templateValidator } } }
   },
   { method: 'GET', path: '/templates/offerAssistance/{file}',
-    handler: { directory: { path: './client/app/offerAssistance' } },
+    handler: { directory: { path: '../client/app/offerAssistance' } },
     config: { validate: { params: { file: templateValidator } } }
   },
   { method: 'GET', path: '/templates/profile/{file}',
-    handler: { directory: { path: './client/app/profile' } },
+    handler: { directory: { path: '../client/app/profile' } },
     config: { validate: { params: { file: templateValidator } } }
   },
   { method: 'GET', path: '/templates/reviewAssistance/{file}',
-    handler: { directory: { path: './client/app/reviewAssistance' } },
+    handler: { directory: { path: '../client/app/reviewAssistance' } },
     config: { validate: { params: { file: templateValidator } } }
   },
 
   // Expose our directive templates
   { method: 'GET', path: '/templates/directives/{file}',
-    handler: { directory: { path: './client/common/directives' } },
+    handler: { directory: { path: '../client/common/directives' } },
     config: { validate: { params: { file: templateValidator } } }
   },
 
   // Expose our bower components so we can get at the css files within
-  { method: 'GET', path: '/bower/{path*}', handler: { directory: { path: './client/bower_components' } } },
+  { method: 'GET', path: '/bower/{path*}', handler: { directory: { path: '../client/bower_components' } } },
 
   // Expose the entry point to the Angular application
-  { method: 'GET', path: '/{path?}', handler: {file: './client/app/_application/app.html'} }
+  { method: 'GET', path: '/{path?}', handler: {file: '../client/app/_application/app.html'} }
 ]);
 
 server.start();
