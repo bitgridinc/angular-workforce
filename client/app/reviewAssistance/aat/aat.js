41ac715330f53bbee9e014f6df9bf2571bdd1ee1
Chris Hairfield <chairfield@gmail.com>
2015-01-16 08:43:38 -0800

    Adding note to myself.

--- client/app/reviewAssistance/aat/aat.js
+++ client/app/reviewAssistance/aat/aat.js
@@ -13,6 +13,8 @@ describe('the offer assistance view', function() {
     reviewAssistanceLocators = new ReviewAssistanceLocators();
   });
 
+  // TODO: Deal with the duplication between this and offerAssistance. Tricky given how this functionality will change
+  // TODO: when it is possible to accept multiple beacons.
   it('should navigate to the list of beacons when an offer is accepted', function() {
     ptor.findElement(reviewAssistanceLocators.acceptButton).click();
     expect(browser.getCurrentUrl()).not.toContain('/review');
