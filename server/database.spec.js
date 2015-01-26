e11a17da847a8a961d906a238da18c0082ffdc11
Chris Hairfield <chairfield@gmail.com>
2015-01-26 08:25:20 -0800

    It's a failing test, but I don't want to start my day here.

--- server/database.spec.js
+++ server/database.spec.js
@@ -6,4 +6,16 @@ describe('the database', function() {
   it('should initialize with three entities defined', function() {
     expect(db.getAllEntities().length).toBe(3);
   });
+
+  it('should save a new beacon', function() {
+    var beacon = {
+      title: 'a',
+      description: 'b',
+      lat: 1,
+      lng: 2
+    };
+
+    db.getAllEntities().get(0);
+    db.createNewBeacon(beacon);
+  });
 });
