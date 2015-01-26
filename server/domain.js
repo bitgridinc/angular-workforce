374961cb6fe710c2b61472e1395ace7df8ffcc49
Chris Hairfield <chairfield@gmail.com>
2015-01-26 08:40:07 -0800

    My basic understanding of the domain is that it will add the responses and accepted assistance arrays.

--- server/domain.js
+++ server/domain.js
@@ -1,42 +1,8 @@
-/*"use strict";
-
-var db = require('./database');
-
-function save(id, value, callback) {
-  console.log('save called', id, value);
-  db.put(id, value, function(err) {
-    if (err) {
-      console.log('Create failed: ', err);
-    }
-    callback(err, value);
-  });
-}
+"use strict";
 
 module.exports = {
-  getBeacons: function(callback){
-    var beacons = [];
-    db.createReadStream()
-      .on('data', function(data) {
-        console.log(data.key, '=', data.value);
-        beacons.push(data.value);
-      })
-      .on('error', function(err) {
-        console.log('Oh my!', err);
-        callback(err, null);
-      })
-      .on('close', function() {
-        console.log('Stream closed');
-        callback(null, beacons);
-      })
-      .on('end', function() {
-        console.log('Stream closed');
-        callback(null, beacons);
-      });
-  },
-  createBeacon: function(organization, title, description, lat, lng, callback){
-    var beaconId = helpers.guid();
+  createBeacon: function(organization, title, description, lat, lng){
     var beacon = {
-      id: beaconId,
       organization: organization,
       title: title,
       description: description,
@@ -45,6 +11,6 @@ module.exports = {
       responses: [],
       acceptedAssistance: []
     };
-    save(beaconId, beacon, callback);
+    return beacon;
   }
-};*/
+};
