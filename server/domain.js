5c70394546305530c48e021d3a41ba923aaba3bf
Chris Hairfield <chairfield@gmail.com>
2015-01-26 08:41:37 -0800

    I think it's part of the domain that beacons are unique and should have a unique id.

--- server/domain.js
+++ server/domain.js
@@ -1,8 +1,12 @@
 "use strict";
 
+var uuid = require('node-uuid');
+
 module.exports = {
   createBeacon: function(organization, title, description, lat, lng){
+    var beaconId = uuid.v4();
     var beacon = {
+      id: beaconId,
       organization: organization,
       title: title,
       description: description,
