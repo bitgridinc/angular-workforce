7b6fb21042eec02304f586991c1a162a727320ed
Chris Hairfield <chairfield@gmail.com>
2015-01-26 13:27:48 -0800

    Built the first half of the CreateBeacon integration test. I verify that my response to the POST is correct, but don't yet test that the new beacon is sent out over socket-io.

--- server/api.spec.js
+++ server/api.spec.js
@@ -1,9 +1,33 @@
 "use strict";
 
-var http = require('http');
+var http = require('http'),
+    request = require('request');
+
+var serverURL = 'http://0.0.0.0:8080';
 
 describe('the create beacon API method', function() {
   it('should exist', function() {
+    request.post(
+      {
+        uri: serverURL + '/beacon',
+        body: JSON.stringify({
+          contents: {
+            title: 'title',
+            description: 'description',
+            lat: 1,
+            lng: 2
+          },
+          senderId: '55a2726e-43ff-4ea9-8d3e-b7c439ef0e84'
+        })
+      },
+      function(error, response, body) {
+        expect(error).toBeNull();
+        var bodyObj = JSON.parse(body);
+        expect(bodyObj.responses).toEqual([]);
+        expect(bodyObj.acceptedAssistance).toEqual([]);
+      }
+    );
 
+    // This should also send a socket-io message
   });
 });
