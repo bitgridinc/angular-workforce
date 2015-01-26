0e6639498a7cfad44d140d67abf190b8a2c53d7b
Chris Hairfield <chairfield@gmail.com>
2015-01-26 08:57:19 -0800

    Pushing repository state into a separate file.

--- server/repository.js
+++ server/repository.js
@@ -1,33 +1,21 @@
 "use strict";
 
-var entities = [
-  {
-    name: 'Your Organization',
-    id: '55a2726e-43ff-4ea9-8d3e-b7c439ef0e84'
-  },
-  {
-    name: 'Their Organization',
-    id: '7cf52dba-992e-4f3f-bbb7-36f4b1792e69'
-  },
-  {
-    name: 'Third Organization',
-    id: 'c1d8d77c-b4d7-4007-a5ea-a0564c751f54'
-  }
-];
-var next = 0;
+var state = require('./repositoryState');
+
+var nextEntity = 0;
 
 module.exports = {
   createBeacon: function(beacon) {
     console.log('repository.createBeacon called.', beacon);
   },
   getAllEntities: function() {
-    return entities
+    return state.entities;
   },
   getCurrentEntity: function () {
-    if (next >= entities.length) {
-      next = 0;
+    if (nextEntity >= state.entities.length) {
+      nextEntity = 0;
     }
 
-    return entities[next++];
+    return state.entities[nextEntity++];
   }
 };
---
+++ server/repositoryState.js
@@ -0,0 +1,18 @@
+"use strict";
+
+module.exports = {
+  entities: [
+    {
+      name: 'Your Organization',
+      id: '55a2726e-43ff-4ea9-8d3e-b7c439ef0e84'
+    },
+    {
+      name: 'Their Organization',
+      id: '7cf52dba-992e-4f3f-bbb7-36f4b1792e69'
+    },
+    {
+      name: 'Third Organization',
+      id: 'c1d8d77c-b4d7-4007-a5ea-a0564c751f54'
+    }
+  ]
+};
