a0e0a588df1e3524eb447bda5fa774c88a934f53
Chris Hairfield <chairfield@gmail.com>
2015-01-26 19:42:51 -0800

    I don't need this anymore.

--- server/api.js
+++ server/api.js
@@ -25,19 +25,6 @@ module.exports = {
         senderId: request.payload.senderId,
         rootMessageId: beacon.id
       });
-
-      /*everything.createBeacon(request.payload.organization, request.payload.title, request.payload.description, request.payload.lat, request.payload.lng, function (err, beacon) {
-        if (err) {
-          console.log('createBeacon handler error');
-          // TODO: Make a constants file
-          var error = Hapi.error.badRequest('Cannot create beacon!');
-          error.output.statusCode = 400;
-          reply(error);
-        } else {
-          console.log('createBeacon handler success');
-          reply(beacon);
-        }
-      });*/
     },
     app: {
       name: 'beacon'
