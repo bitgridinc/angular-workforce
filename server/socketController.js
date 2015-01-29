be5c5ef75564a5447d19e0fb5f0bfcc0f6833536
Chris Hairfield <chairfield@gmail.com>
2015-01-29 14:51:53 -0800

    I now know how to address specific recipients.

--- server/socketController.js
+++ server/socketController.js
@@ -4,10 +4,14 @@
 var io = require('./socketSetup').instance;
 var storage = require('./storage');
 
-io.sockets.on('connection', function(socket){
-  socket.emit('init', {
+io.sockets.on('connection', function(client){
+  var clientEntity = storage.getCurrentEntity();
+  client.join(clientEntity.id);
+
+  // While I could just do client.emit(..., this is useful way to remembering how to address a specific client.
+  io.to(clientEntity.id).emit('init', {
     allEntities: storage.getAllEntities(),
-    currentEntity: storage.getCurrentEntity(),
+    currentEntity: clientEntity,
     beacons: storage.getAllBeacons()
   });
 });
