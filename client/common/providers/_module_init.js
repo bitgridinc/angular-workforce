0fb996b3ada40667c32a06d0bd3e2bfbba571afe
Chris Hairfield <chairfield@gmail.com>
2015-01-29 14:48:49 -0800

    Removing the MessageSenderService.

--- client/common/providers/_module_init.js
+++ client/common/providers/_module_init.js
@@ -6,7 +6,6 @@ module.exports = angular.module('modules.providers', []);
 
 require('./authenticationService.js');
 require('./messagePacketizerService.js');
-require('./messageSenderService.js');
 require('./paginationControlFactory.js');
 require('./restService.js');
 require('./socketFactory.js');
--- server/socketController.js
+++ server/socketController.js
@@ -2,7 +2,6 @@
 'use strict';
 
 var io = require('./socketSetup').instance;
-var uuid = require('node-uuid');
 var storage = require('./storage');
 
 io.sockets.on('connection', function(socket){
@@ -10,22 +9,5 @@ io.sockets.on('connection', function(socket){
     allEntities: storage.getAllEntities(),
     currentEntity: storage.getCurrentEntity(),
     beacons: storage.getAllBeacons()
-  });
-
-  socket.on('message', function(message) {
-    if (message.contents === null || message.contents === undefined) {
-      console.log('Message didn\'t contain a contents property. Ignoring message.');
-    }
-    else {
-      // The id is the unique key used to handle selection in the UI
-      message.contents.id = uuid.v4();
-
-      if (message.rootMessageId === undefined) {
-        message.rootMessageId = message.contents.id;
-      }
-
-      // This sends back to the sender as well
-      io.sockets.emit('message', message);
-    }
   });
 });
