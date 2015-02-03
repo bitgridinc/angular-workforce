909ade0fb79613f1bbf0c3e5851c2e48d42e249d
Chris Hairfield <chairfield@gmail.com>
2015-02-03 10:58:40 -0800

    Centering map on NY.

--- server/storageState.js
+++ server/storageState.js
@@ -21,7 +21,7 @@ module.exports = {
     factories.newBeaconFactory()
       .withIds(117, '55a2726e-43ff-4ea9-8d3e-b7c439ef0e84') // Tupper Lake
       .withSummaryText('Your Title', 'Your Description')
-      .withLocation(38.9, -77.0)
+      .withLocation(44.248961, -74.485675)
       .withResponse(factories.newAssistanceResponseFactory()
         .withIds('2cf8faaa-5760-41c9-adbf-5a4482ac3469', '7cf52dba-992e-4f3f-bbb7-36f4b1792e69', 117)
         .withResponderCrew(4, new Date())
@@ -30,7 +30,7 @@ module.exports = {
     factories.newBeaconFactory()
       .withIds(1337, '7cf52dba-992e-4f3f-bbb7-36f4b1792e69') // Silver Springs
       .withSummaryText('Their Title', 'Their Description')
-      .withLocation(38.88, -77.02)
+      .withLocation(42.658379, -78.081735)
       .createBeacon()
   ]
 };
