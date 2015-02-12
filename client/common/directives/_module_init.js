41127396e82ad4f738b7b1c04eabd5e6700556d3
Chris Hairfield <chairfield@gmail.com>
2015-02-12 10:42:39 -0800

    Creating a container with and icon on the left to be used for input groups.

--- client/common/directives/_module_init.js
+++ client/common/directives/_module_init.js
@@ -7,5 +7,6 @@ module.exports = angular.module('modules.directives', []);
 require('./acceptedAssistanceSummary.js');
 require('./closeablePanel.js');
 require('./coloredContainer.js');
+require('./iconContainer.js');
 require('./openReviewAssistanceButton.js');
 require('./organizationLabel.js');
---
+++ client/common/directives/iconContainer.js
@@ -0,0 +1,20 @@
+"use strict";
+
+require('./_module_init.js')
+  .directive('iconContainer',
+    function() {
+      return {
+        restrict: 'E',
+        templateUrl: '/templates/directives/iconContainer.tpl.html',
+        transclude: true,
+        scope: {
+          iconUrl: '@'
+        },
+        link: function(scope, element, attrs) {
+          if (angular.isDefined(scope.height)) {
+            element.css('height', scope.height + 'px');
+          }
+        }
+      }
+    }
+  );
