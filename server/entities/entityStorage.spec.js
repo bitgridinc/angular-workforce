b8eb0f18d1eb7714597e4d19f7c4f245698974c0
Chris Hairfield <chairfield@gmail.com>
2015-05-02 11:43:27 -0400

    It always misses files...

--- server/entities/entityStorage.spec.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var db = require('./entityStorage');
-
-describe('the entity storage', function() {
-  it('should initialize with three entities defined', function() {
-    expect(db.getAllEntities().length).toBe(4);
-  });
-});
