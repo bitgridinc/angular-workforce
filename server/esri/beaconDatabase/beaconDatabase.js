0b68e2aa0b1a80debb623f1e74a0a4f5f990ba08
Chris Hairfield <chairfield@gmail.com>
2015-05-03 10:39:32 -0400

    Trivial refactor

--- server/esri/beaconDatabase/beaconDatabase.js
+++ server/esri/beaconDatabase/beaconDatabase.js
@@ -3,23 +3,28 @@
 var hardcodedBeaconDatabase = require('./beaconDatabase.hardcoded.js'),
     esriBeaconDatabase = require('./beaconDatabase.esri.js');
 
+function runningForAATs() {
+  // The presence of the aat env var indicates we're running for our AATs
+  return process.env.aat;
+}
+
 module.exports = {
   saveBeacon: function(beacon, successCallback) {
-    if (process.env.aat) {
+    if (runningForAATs()) {
       hardcodedBeaconDatabase.saveBeacon(beacon, successCallback);
     } else {
       esriBeaconDatabase.saveBeacon(beacon, successCallback);
     }
   },
   getAllBeacons: function(successCallback) {
-    if (process.env.aat) {
+    if (runningForAATs()) {
       hardcodedBeaconDatabase.getAllBeacons(successCallback);
     } else {
       esriBeaconDatabase.getAllBeacons(successCallback);
     }
   },
   getBeaconById: function(id, successCallback) {
-    if (process.env.aat) {
+    if (runningForAATs()) {
       hardcodedBeaconDatabase.getBeaconById(id, successCallback);
     } else {
       esriBeaconDatabase.getBeaconById(id, successCallback);
