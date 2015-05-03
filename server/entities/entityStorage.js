e9e6a363e2de0c75f306cb80b90ab19ef98750e9
Chris Hairfield <chairfield@gmail.com>
2015-05-03 10:37:47 -0400

    getCurrentEntity now returns same entity during AAT

--- server/entities/entityStorage.js
+++ server/entities/entityStorage.js
@@ -7,7 +7,12 @@ module.exports = {
   getAllEntities: function() {
     return entityDatabase.entities;
   },
-  getCurrentEntity: function () {
+  getCurrentEntity: function() {
+    if (process.env.aat) {
+      console.log('HERE', process.env);
+      return entityDatabase.entities[1];
+    }
+
     if (nextEntity >= entityDatabase.entities.length) {
       nextEntity = 0;
     }
