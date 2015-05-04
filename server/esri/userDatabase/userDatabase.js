31535da6dec798a9a8996aa2ef2fcd6a460f76c1
Chris Hairfield <chairfield@gmail.com>
2015-05-03 19:28:12 -0700

    The users database shouldn't be hit if we're running aats.

--- server/esri/userDatabase/userDatabase.js
+++ server/esri/userDatabase/userDatabase.js
@@ -5,6 +5,10 @@ var AGO = require('esri-portal-api');
 
 module.exports = {
   getAllUsers: function() {
+    if (process.env.aat) {
+      return [];
+    }
+
     var ago = new AGO();
     return ago.portal.users(
       'Gz4_3fTmMIGWtiiIHvFxo-fM6x9qcBvaV8YsSNTHDvoj8M-7I_3Vj4zP3HYgaayZhHa8jHCMsm62x_0FlTbARe7wB76K4wea3pHMQUcThnYvywVOi6vBRROr6rPr2O1sXDmvJp4nU_2JPw3Casi0CgeEnZMSsbW8bvRp1agwkSU7eMYGyWOJFTeGKC0_scrT',
---
+++ server/spec/userDatabase.spec.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var db = require('../esri/userDatabase/userDatabase');
+
+describe('the user storage', function() {
+  describe('when the aat env var is present', function() {
+    beforeEach(function() {
+      // Arrange the environment to use our hardcoded data instead of hitting ArcGIS Online
+      process.env.aat = true;
+    });
+
+    it('should return no users', function() {
+      // Act by getting all users
+      var users = db.getAllUsers();
+
+      // Assert there are none
+      expect(users.length).toBe(0);
+    });
+  });
+});
