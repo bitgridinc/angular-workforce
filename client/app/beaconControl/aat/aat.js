dda0fb2e1eb704bbff75be3fc16ceb881f1ccc89
Chris Hairfield <chairfield@gmail.com>
2015-05-19 13:20:18 -0700

    Testing new Protractor API.

--- client/app/beaconControl/aat/aat.js
+++ client/app/beaconControl/aat/aat.js
@@ -4,27 +4,25 @@ var BeaconControlLocators = require('./locators.js')
   , ListBeaconsLocators = require('../../listBeacons/aat/locators.js');
 
 describe('the main dashboard', function() {
-  var ptor
-    , beaconControlLocators
+  var beaconControlLocators
     , listBeaconsLocators;
 
   beforeEach(function() {
-    ptor = protractor.getInstance();
     beaconControlLocators = new BeaconControlLocators();
     listBeaconsLocators = new ListBeaconsLocators();
   });
 
   it('should open and close the list of beacons when the My Beacons button is clicked', function() {
     // Arrange
-    ptor.get('/#/dashboard');
+    browser.get('/#/dashboard');
 
     // Act/Assert for the first click
-    ptor.findElement(beaconControlLocators.myBeaconsButton).click();
+    browser.findElement(beaconControlLocators.myBeaconsButton).click();
     expect(browser.getCurrentUrl()).toContain('/#/dashboard/beacons');
     expect(ptor.isElementPresent(listBeaconsLocators.createBeaconButton)).toBeTruthy();
 
     // Act/Assert for the second click
-    ptor.findElement(beaconControlLocators.myBeaconsButton).click();
+    browser.findElement(beaconControlLocators.myBeaconsButton).click();
     expect(browser.getCurrentUrl()).toContain('/#/dashboard');
     expect(browser.getCurrentUrl()).not.toContain('/#/dashboard/beacons');
     expect(ptor.isElementPresent(listBeaconsLocators.createBeaconButton)).toBeFalsy();
