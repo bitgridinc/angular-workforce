5055522d6b00f3f4781c034f31e2e39ef94bcfba
Chris Hairfield <chairfield@gmail.com>
2015-05-27 11:22:13 -0700

    Trivial: removing tests against our test code.

--- server/spec/organizationDatabase.spec.js
+++ server/spec/organizationDatabase.spec.js
@@ -4,7 +4,7 @@ var db = require('../inMemory/organizations/organizationDatabase')
   , environment = require('../environment.js');
 
 describe('the organization database', function() {
-  describe('when the aat env var is not present', function() {
+  describe('when in production mode', function() {
     beforeEach(function() {
       environment.changeToProductionMode();
     });
@@ -19,29 +19,6 @@ describe('the organization database', function() {
 
       // Assert that they are the same
       expect(firstCallOrganization).not.toBe(secondCallOrganization);
-    });
-  });
-
-  describe('when the aat env var is present', function() {
-    beforeEach(function() {
-      environment.changeToTestMode();
-    });
-
-    // This is necessary as I have code to switch the current organization for demo and hand-testing reasons
-    it('should return the same organization on subsequent calls', function() {
-      // Act by calling getCurrentOrganization twice
-      var firstCallOrganization = db.getCurrentOrganization();
-      var secondCallOrganization = db.getCurrentOrganization();
-
-      // Assert that they are the same
-      expect(firstCallOrganization).toBe(secondCallOrganization);
-    });
-    it('should not return the Murfreesboro organization as our AATs would fail', function() {
-      // Act by getting the organization
-      var organization = db.getCurrentOrganization();
-
-      // Assert
-      expect(organization.name).not.toContain('Murfreesboro');
     });
   });
 });
