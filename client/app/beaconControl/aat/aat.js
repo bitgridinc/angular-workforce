3a6ccd1bdd889c00a6d3f719bb565d1cf9a40e62
Chris Hairfield <chairfield@gmail.com>
2015-06-01 16:22:09 -0700

    While the tests aren't done, I just found out how to place the new and require on the same line. I'm going to implement this everywhere.

--- client/app/beaconControl/aat/aat.js
+++ client/app/beaconControl/aat/aat.js
@@ -1,15 +1,47 @@
 "use strict";
 
-var BeaconControlLocators = require('./locators.js')
-  , ListBeaconsLocators = require('../../listBeacons/aat/locators.js');
+var beaconControlLocators = new (require('./locators.js'))()
+  , listBeaconsLocators = new (require('../../listBeacons/aat/locators.js'))();
 
-describe('the main dashboard', function() {
-  var beaconControlLocators
-    , listBeaconsLocators;
+function clickControl() {
+  browser.findElement(beaconControlLocators.myBeaconsButton).click();
+}
 
-  beforeEach(function() {
-    beaconControlLocators = new BeaconControlLocators();
-    listBeaconsLocators = new ListBeaconsLocators();
+describe('the beacon control', function() {
+  describe('when not expanded', function() {
+    beforeEach(function() {
+      browser.get('/#/dashboard');
+    });
+    it('is visible without an arrow icon (visual)', function() {
+
+    });
+    it('does not display the contents of the control (e.g., beacon list)', function() {
+    });
+    describe('when clicked', function() {
+      beforeEach(function() { clickControl(); });
+      it('opens the contents of the control', function() {
+      });
+      it('navigates to the correct url', function() {
+        expect(browser.getCurrentUrl()).toMatch('/#/dashboard/beacons$');
+      });
+    });
+  });
+  describe('when expanded', function() {
+    beforeEach(function() {
+      browser.get('/#/dashboard/beacons');
+    });
+    it('is visible with an arrow icon (visual)', function() {
+    });
+    it('displays the contents of the control (e.g., beacon list)', function() {
+    });
+    describe('when clicked', function() {
+      beforeEach(function() { clickControl(); });
+      it('closes the contents of the control', function() {
+      });
+      it('navigates to the correct url', function() {
+        expect(browser.getCurrentUrl()).toMatch('/#/dashboard$');
+      });
+    });
   });
 
   it('should open and close the list of beacons when the My Beacons button is clicked', function() {
