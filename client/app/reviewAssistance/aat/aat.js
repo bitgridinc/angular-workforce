f7c2410943f89d8750e37cd94298e3837109f339
Chris Hairfield <chairfield@gmail.com>
2015-06-02 11:54:47 -0700

    Building out the beginnings of the review offers AATs. I'm held up by the bug with the switcher.

--- client/app/reviewAssistance/aat/aat.js
+++ client/app/reviewAssistance/aat/aat.js
@@ -3,15 +3,52 @@
 
 var reviewAssistanceLocators = new (require('./locators.js'))();
 
+function expectOfferInformationDisplayedToBeTruthy(organizationName) {
+  var organizationCard = element(reviewAssistanceLocators.organizationCard);
+  expect(organizationCard.getText()).toContain(organizationName);
+
+  var contactCard = element(reviewAssistanceLocators.contactCard);
+  expect(contactCard.getText()).toMatch(/\d{4}$/); // because seeing 4 digits at the end is a good phone number approximation
+
+  var organizationLabelElement = element(reviewAssistanceLocators.organizationLabel);
+  expect(organizationLabelElement.getText()).toContain(organizationName);
+  expect(organizationLabelElement.getText()).toMatch(/with .+ people/);
+  expect(organizationLabelElement.getText()).toMatch(/20\d\d/);
+}
+function expectAcceptButtonIsPresent() {
+  return expect(browser.isElementPresent(reviewAssistanceLocators.acceptButton));
+}
+
 describe('the review assistance view', function() {
-  beforeEach(function() {
-    browser.get('/#/dashboard/beacons/30/review/2cf8faaa-5760-41c9-adbf-5a4482ac3469');
+  describe('when reviewing offers for beacon 30, which was sent by another utility,', function() {
+    beforeEach(function() {
+      browser.get('/#/dashboard/beacons/30/review/2cf8faaa-5760-41c9-adbf-5a4482ac3469');
+    });
+
+    it('should display the name of the organization that offered assistance', function() {
+      expectOfferInformationDisplayedToBeTruthy('Morristown Utility Systems');
+    });
+    it('should not display the button to accept the assistance since it is not the user\'s beacon', function() {
+      expectAcceptButtonIsPresent().toBeFalsy();
+    });
   });
 
-  it('should display the name of the organization that offered assistance', function() {
-    var organizationLabelElement = element(reviewAssistanceLocators.organizationLabel);
-    expect(organizationLabelElement.getText()).toContain('Morristown Utility Systems');
-    expect(organizationLabelElement.getText()).toContain('with 4 people');
-    expect(organizationLabelElement.getText()).toContain('2015');
+  describe('when reviewing offers for beacon 32, which was sent by the user\'s utility,', function() {
+    beforeEach(function() {
+      browser.get('/#/dashboard/beacons/32/review/eb6cd1ad-d115-49de-aac0-cfbb887d9ad0');
+    });
+
+    it('should display the name of the organization that offered assistance', function() {
+      expectOfferInformationDisplayedToBeTruthy('Murfreesboro Electric Department');
+    });
+    it('should display the button to accept the assistance since it is the user\'s beacon', function() {
+      expectAcceptButtonIsPresent().toBeTruthy();
+    });
+
+    describe('when viewing the second offer', function() {
+      beforeEach(function() {
+        
+      });
+    });
   });
 });
--- client/app/reviewAssistance/aat/locators.js
+++ client/app/reviewAssistance/aat/locators.js
@@ -1,6 +1,8 @@
 "use strict";
 
 var reviewAssistanceLocators = function() {
+  this.organizationCard = by.id('bg-ra-organization-card');
+  this.contactCard = by.id('bg-ra-contact-card');
   this.organizationLabel = by.id('bg-ra-offer-summary');
   this.acceptButton = by.id('bg-ra-accept-button');
 };
