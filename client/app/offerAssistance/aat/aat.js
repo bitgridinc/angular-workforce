165ea44d9f849445d7cfc159e740cc47ec93e8b8
Chris Hairfield <chairfield@gmail.com>
2015-06-02 15:46:03 -0700

    Adding AAT coverage for offering assistance to beacon 34.

--- client/app/offerAssistance/aat/aat.js
+++ client/app/offerAssistance/aat/aat.js
@@ -1,22 +1,49 @@
 "use strict";
 
-var offerAssistanceLocators = new (require('./locators.js'))();
+var offerAssistanceLocators = new (require('./locators.js'))()
+  , directiveLocators = new (require('../../../common/directives/aat/locators.js'))();
+
+function assistBeacon(beaconId) {
+  browser.get('/#/dashboard/beacons/' + beaconId + '/assist');
+}
+function clickButtonToOfferAssistance() {
+  browser.findElement(offerAssistanceLocators.assistButton).click();
+}
 
 describe('the offer assistance view', function() {
-  beforeEach(function() {
-    browser.get('/#/dashboard/beacons/30/assist');
+  describe('when offering assistance to beacon 30', function() {
+    beforeEach(function() {
+      assistBeacon(30);
+    });
+
+    // TODO: I'd like some way to ensure that there is a defined beacon in scope
+    /*it('should display the name of the company you\'re offering to help', function() {
+     expect(element(by.text('has requested')).isPresent()).toBeTruthy();
+     });*/
+
+    it('should navigate to the list of beacons when an offer is sent', function() {
+      clickButtonToOfferAssistance();
+      expect(browser.getCurrentUrl()).not.toContain('/assist');
+      expect(browser.getCurrentUrl()).not.toContain('/detail');
+      expect(browser.getCurrentUrl()).toContain('/#/dashboard/beacons');
+      // TODO: Ensure something changes on the UI. This should not be checking whether the review offers button appears.
+    });
   });
 
-  // TODO: I'd like some way to ensure that there is a defined beacon in scope
-  /*it('should display the name of the company you\'re offering to help', function() {
-    expect(element(by.text('has requested')).isPresent()).toBeTruthy();
-  });*/
+  describe('when offering assistance to beacon 34', function() {
+    beforeEach(function() {
+      assistBeacon(34);
+    });
 
-  it('should navigate to the list of beacons when an offer is sent', function() {
-    browser.findElement(offerAssistanceLocators.assistButton).click();
-    expect(browser.getCurrentUrl()).not.toContain('/assist');
-    expect(browser.getCurrentUrl()).not.toContain('/detail');
-    expect(browser.getCurrentUrl()).toContain('/#/dashboard/beacons');
-    // TODO: Ensure something changes on the UI. This should not be checking whether the review offers button appears.
+    describe('submitting the offer', function() {
+      beforeEach(function() {
+        clickButtonToOfferAssistance();
+      });
+
+      it('should navigate the user back to the beacon list where a third review offers button appears', function() {
+        expect(browser.getCurrentUrl()).toMatch('/#/dashboard/beacons$');
+        expect(element.all(directiveLocators.reviewOffersButton).count()).toBe(3);
+      });
+    });
   });
 });
