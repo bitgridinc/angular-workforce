8bd56978b4d615fa17d7044c8efc07cae5443876
Chris Hairfield <chairfield@gmail.com>
2015-06-03 10:33:08 -0700

    Moved the validation logic into RestService. Still have to unit test the POST.

--- client/common/providers/restService/restService.js
+++ client/common/providers/restService/restService.js
@@ -4,12 +4,24 @@ var apiRoutes = require('../../../../shared/apiRoutes');
 
 require('./../_module_init.js')
   .service('RestService',
-    [         '$http',
-      function($http) {
+    [         '_', '$http',
+      function(_,   $http) {
         return {
-          createBeacon: function(newBeaconData) {
-            console.log('Sending data to create a new beacon:', newBeaconData);
-            $http.post(apiRoutes.createBeacon, newBeaconData);
+          createBeacon: function(newBeaconData, errorCallback) {
+            var errors = _.filter(['title', 'description', 'streetAddress', 'zip', 'numberOfPeople'], function(requiredProperty) {
+              /* jshint -W116 */
+              return newBeaconData[requiredProperty] == null || // Type coercion covers both null and undefined
+              /* jshint +W116 */
+                     newBeaconData[requiredProperty] === '';
+            });
+
+            if (errors.length > 0) {
+              console.log('New beacon data had the following errors:', newBeaconData, errors);
+              errorCallback(errors);
+            } else {
+              console.log('Sending data to create a new beacon:', newBeaconData);
+              $http.post(apiRoutes.createBeacon, newBeaconData);
+            }
           },
           offerAssistance: function(newOfferData) {
             console.log('Sending data to create a new offer:', newOfferData);
