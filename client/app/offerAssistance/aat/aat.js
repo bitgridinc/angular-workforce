92fec4494fe3bc287bb3dfea70124dc0e843b69b
Chris Hairfield <chairfield@gmail.com>
2015-06-04 07:20:57 -0700

    Finished the AATs for offer assistance.

--- client/app/offerAssistance/aat/aat.js
+++ client/app/offerAssistance/aat/aat.js
@@ -3,48 +3,33 @@
 var offerAssistanceLocators = new (require('./locators.js'))()
   , directiveLocators = new (require('../../../common/directives/aat/locators.js'))();
 
-function assistBeacon(beaconId) {
-  browser.get('/#/dashboard/beacons/' + beaconId + '/assist');
-}
-function clickButtonToOfferAssistance() {
-  browser.findElement(offerAssistanceLocators.assistButton).click();
-}
-
 describe('the offer assistance view', function() {
-  describe('when offering assistance to beacon 30', function() {
+  describe('when offering assistance to beacon 34', function() {
     beforeEach(function() {
       // Arrange
-      assistBeacon(30);
+      browser.get('/#/dashboard/beacons/34/assist');
     });
 
+    it('should display the details of the beacon being assisted', function() {
+      // Assert that the binding was populated properly
+      expect(element(offerAssistanceLocators.headerParagraph).getText()).toMatch(/^Murfreesboro Electric Department has/);
+    });
     it('should allow for selecting a date through the calendar', function() {
-      // Act
+      // Act by opening the calendar and selecting the date as today
       browser.findElement(offerAssistanceLocators.toggleCalendarButton).click();
+      browser.findElement(offerAssistanceLocators.calendarTodayButton).click();
+
+      // Assert
+      expect(element(offerAssistanceLocators.calendarTodayButton).isDisplayed()).toBeFalsy();
+      expect(element(offerAssistanceLocators.dateInput).getAttribute('value')).toMatch(/\d\d?\/\d\d?\/\d\d$/);
     });
+    it('submitting an offer to beacon 34 should navigate the user back to the beacon list where a third review offers button appears', function() {
+      // Act
+      browser.findElement(offerAssistanceLocators.assistButton).click();
 
-    // TODO: I'd like some way to ensure that there is a defined beacon in scope
-    /*it('should display the name of the company you\'re offering to help', function() {
-     expect(element(by.text('has requested')).isPresent()).toBeTruthy();
-     });*/
-
-    it('should navigate to the list of beacons when an offer is sent', function() {
-      clickButtonToOfferAssistance();
-      expect(browser.getCurrentUrl()).not.toContain('/assist');
-      expect(browser.getCurrentUrl()).not.toContain('/detail');
-      expect(browser.getCurrentUrl()).toContain('/#/dashboard/beacons');
-      // TODO: Ensure something changes on the UI. This should not be checking whether the review offers button appears.
+      // Assert
+      expect(browser.getCurrentUrl()).toMatch('/#/dashboard/beacons$');
+      expect(element.all(directiveLocators.reviewOffersButton).count()).toBe(3);
     });
-  });
-
-  it('submitting an offer to beacon 34 should navigate the user back to the beacon list where a third review offers button appears', function() {
-    // Arrange
-    assistBeacon(34);
-
-    // Act
-    clickButtonToOfferAssistance();
-
-    // Assert
-    expect(browser.getCurrentUrl()).toMatch('/#/dashboard/beacons$');
-    expect(element.all(directiveLocators.reviewOffersButton).count()).toBe(3);
   });
 });
--- client/app/offerAssistance/aat/locators.js
+++ client/app/offerAssistance/aat/locators.js
@@ -3,7 +3,11 @@
 var offerAssistanceLocators = function() {
   this.assistButton = by.buttonText('Assist');
   this.declineButton = by.buttonText('Stuff \'Em');
+  this.headerParagraph = by.id('bg-oa-header-paragraph');
+  this.numRespondersInput = by.id('bg-oa-num-responders-input');
+  this.dateInput = by.id('bg-oa-date-input');
   this.toggleCalendarButton = by.id('bg-oa-toggle-calendar');
+  this.calendarTodayButton = by.buttonText('Today');
 };
 
 module.exports = offerAssistanceLocators;
