15d5ac6e0185631c0692f1523de4aa810c0ce3cc
Chris Hairfield <chairfield@gmail.com>
2015-06-04 14:08:39 -0700

    Trivial commenting.

--- client/app/_application/aat/aat.js
+++ client/app/_application/aat/aat.js
@@ -2,10 +2,14 @@
 
 describe('the application', function() {
   it('should redirect to homepage when / is accessed', function() {
+    // Arrange
     browser.get('/#/dashboard');
     var dashboardUrl = browser.getCurrentUrl();
 
+    // Act
     browser.get('/');
+
+    // Assert
     expect(browser.getCurrentUrl()).toBe(dashboardUrl);
   });
 });
