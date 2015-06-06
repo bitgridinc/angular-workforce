1063e34e8f2af8fee3ae9d5b31e02ac7e6551431
Chris Hairfield <chairfield@gmail.com>
2015-06-06 11:22:16 -0700

    Finished listBeacons AATs.

--- client/app/listBeacons/aat/aat.js
+++ client/app/listBeacons/aat/aat.js
@@ -9,8 +9,11 @@ describe('having the My Beacons button clicked to view the list of existing beac
   });
 
   it('should allow the user to review offers of assistance without having to view the beacon details first', function() {
-    element.all(directiveLocators.reviewOffersButton).get(0).click();
-    expect(browser.getCurrentUrl()).toMatch('/#/dashboard/beacons/30/review/2cf8faaa-5760-41c9-adbf-5a4482ac3469$');
+    // Act
+    element.all(directiveLocators.reviewOffersButton).get(1).click();
+
+    // Assert
+    expect(browser.getCurrentUrl()).toMatch('/#/dashboard/beacons/32/review/eb6cd1ad-d115-49de-aac0-cfbb887d9ad0$');
   });
   it('should only show 4/5 beacons (1 filtered out)', function() {
     expect(element.all(locators.beaconSummaryDirective).count()).toBe(4);
@@ -18,21 +21,21 @@ describe('having the My Beacons button clicked to view the list of existing beac
   it('should only show 2 review offers buttons', function() {
     expect(element.all(directiveLocators.reviewOffersButton).count()).toBe(2);
   });
+  it('clicking the third beacon should expand it to display details', function() {
+    // Act
+    element.all(locators.beaconSummaryDirective).get(2).click();
+
+    // Assert
+    expect(browser.getCurrentUrl()).toMatch('/#/dashboard/beacons/33$');
+    expect(browser.isElementPresent(locators.createBeaconButton)).toBeFalsy();
+  });
   it('clicking the Create Beacon button should navigate to the create beacon view', function() {
+    // Act
     browser.findElement(locators.createBeaconButton).click();
+
+    // Assert
     expect(browser.getCurrentUrl()).toMatch('/#/dashboard/beacons/create$');
     // Note that we don't have logic for clicking the Create Beacon button twice, so it shouldn't be clickable.
     expect(browser.isElementPresent(locators.createBeaconButton)).toBeFalsy();
-  });
-
-  describe('clicking an existing beacon', function() {
-    beforeEach(function() {
-      element.all(locators.beaconSummaryDirective).get(0).click();
-    });
-
-    it('should expand the beacon to view its details', function() {
-      expect(browser.getCurrentUrl()).toMatch('/#/dashboard/beacons/30$');
-      expect(browser.isElementPresent(locators.createBeaconButton)).toBeFalsy();
-    });
   });
 });
