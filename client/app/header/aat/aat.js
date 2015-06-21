1a44f75c4adf0ed5a55e36e1acc5d87883a60384
Chris Hairfield <chairfield@gmail.com>
2015-06-21 11:27:46 -0700

    fixed AAT

--- client/app/header/aat/aat.js
+++ client/app/header/aat/aat.js
@@ -6,7 +6,7 @@ var locators = new (require('./locators.js'))()
 aatWrappers.authenticationRequiredWrapper('the header', function(testRunner, suiteRunner) {
   suiteRunner('/#/dashboard', 'when reviewing offers for beacon 30, which was sent by another utility,', function() {
     it('should display the utility name', function() {
-      expect(element(locators.leftText).getText()).toMatch('as Morristown Utility Systems$');
+      expect(element(locators.leftText).getText()).toMatch('Morristown Utility Systems$');
     });
   });
 });
