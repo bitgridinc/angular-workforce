c2aeafb69521b74f3c025c1a40291531ec22044f
Chris Hairfield <chairfield@gmail.com>
2015-07-01 13:42:28 -0700

    commenting out an annoying console.log and leaving todo for later

--- client/app/listBeacons/requiresInputFilter.js
+++ client/app/listBeacons/requiresInputFilter.js
@@ -5,7 +5,8 @@ require('./_module')
     [         '_', '$rootScope',
       function(_,   $rootScope) {
         return function(beacons) {
-          console.log("Requires Input: ", beacons);
+          // TODO: Why does this keep getting called?
+          // console.log("Requires Input: ", beacons);
           return _.filter(beacons, function(beacon) {
             // We should only see beacons from other utilities or our own that have responses with none yet accepted
             return beacon.senderId !== $rootScope.dataFromServer.currentOrganization.id ||
