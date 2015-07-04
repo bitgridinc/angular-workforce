12c1d639b5b05528459e9c50499d025cf0f219d4
Chris Hairfield <chairfield@gmail.com>
2015-07-04 12:52:34 -0700

    I don't think I need proxyquire

--- server/modules/geodesy/spec/api.integration.spec.js
+++ server/modules/geodesy/spec/api.integration.spec.js
@@ -2,7 +2,7 @@
 
 var environment = require('../../../environment')
   , hapifyPost = require('../../../spec/support/hapiHelpers').hapifyPost
-  , proxyquire = require('proxyquire');
+  , handlers = require('../api');
 
 describe('in production,', function() {
   var mode;
@@ -14,14 +14,8 @@ describe('in production,', function() {
     environment.changeToMode(mode);
   }); // Reset back to whatever the mode was before the test was run
 
-  var handlers;
-  beforeEach(function() {
-    // We must create the spies before we require in the api, as it is in the api's require statements that the
-    // code in which we are spying is used
-    handlers = proxyquire('../api', {});
-  }); // Instantiate our api, the SUT, with the spies set up above
-
-  it('should reply an empty object', function() {
+  it('should call reply', function() {
+    // Arrange
     var reply = jasmine.createSpy();
 
     // Act
