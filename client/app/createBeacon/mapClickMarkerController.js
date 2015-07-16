c86c0f9112ff0ad68620764b81395fe1a62b4a0c
Chris Hairfield <chairfield@gmail.com>
2015-07-15 21:42:22 -0700

    crazy refactor along with new map functionality
    The icon to indicate where the new beacon is to be created now
    disappears when the user cancels. The user also can no longer update the
    location after they are no longer on the createBeacon view.

--- client/app/createBeacon/mapClickMarkerController.js
+++ client/app/createBeacon/mapClickMarkerController.js
@@ -1,25 +1,40 @@
 "use strict";
 
 // TODO: Test this!
-require('./_module')
-  .controller('MapClickMarkerController',
-    [         'leafletData', '$window',
-      function(leafletData,   $window) {
-        var clickMarker
-          , clickLatlng;
-        leafletData.getMap('leaflet').then(function(map) {
-          map.on('click', function(args) {
-            if (clickLatlng) {
-              clickLatlng.lat = args.latlng.lat;
-              clickLatlng.lng = args.latlng.lng;
-              clickMarker.update();
-            } else {
-              clickLatlng = args.latlng;
-              clickMarker = $window.L.marker(clickLatlng);
-              clickMarker.addTo(map);
-            }
-          });
-        });
-      }
-    ]
-  );
+var MapClickMarkerController = function($scope, $window, leafletData) {
+  var _this = this
+    , newBeaconIcon = L.icon({ // Create the utility HQ icon once
+        iconUrl: '/images/plus.png',
+        iconSize: [32, 32]
+      });
+
+  _this.createMarker = function(latlng) {
+    return $window.L.marker(latlng, {
+      icon: newBeaconIcon
+    });
+  };
+
+  leafletData.getMap('leaflet').then(function(map) {
+    var handler = _this.onClick.bind(_this, map);
+
+    map.on('click', handler);
+
+    $scope.$on('$destroy', function() {
+      map.off('click', handler);
+      map.removeLayer(_this.clickMarker);
+    });
+  });
+};
+
+MapClickMarkerController.prototype.onClick = function(map, args) {
+  if (this.clickMarker) {
+    this.clickMarker.setLatLng(args.latlng);
+  } else {
+    this.clickMarker = this.createMarker(args.latlng);
+    this.clickMarker.addTo(map);
+  }
+};
+
+MapClickMarkerController.$inject = ['$scope', '$window', 'leafletData'];
+
+require('./_module').controller('MapClickMarkerController', MapClickMarkerController);
