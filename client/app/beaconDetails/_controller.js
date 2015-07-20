d4e7b79e15f5013766f00f3ae8b6666d1b5b4f3f
Chris Hairfield <chairfield@gmail.com>
2015-07-20 13:27:15 -0500

    removing scope soup

--- client/app/beaconDetails/_controller.js
+++ client/app/beaconDetails/_controller.js
@@ -1,35 +1,28 @@
 "use strict";
 
-var BeaconDetailsController = function () {
+var BeaconDetailsController = function ($rootScope, $scope) {
+  // Note that the OfferAssistance and ReviewAssistance views are not nested
+  // under the BeaconDetails view, so we must use $rootScope instead of $scope.
+  $rootScope.selectionState = {
+    currentBeacon: undefined
+  };
 
+  var cleanup;
+  cleanup = $scope.$watch('dataFromServer.beacons.length', function(newVal, oldVal) {
+    console.log('The number of beacons changed', newVal, oldVal);
+    $scope.selectionState.currentBeacon = $scope.findBeaconById($scope.$stateParams.id);
+    $scope.$on('$destroy', function() {
+      cleanup();
+    });
+  });
+
+  $scope.goToReviewAssistance = function() {
+    $scope.navigationService.navigateTo(
+      'dashboard.beacons.detail.review.response',
+      { responseId: $scope.selectionState.currentBeacon.responses[0].id });
+  };
 };
 
-require('./_module')
-  .controller('BeaconDetailsController',
-    [         '$rootScope', '$scope',
-      function($rootScope,   $scope) {
-        var cleanup;
+BeaconDetailsController.$inject = ['$rootScope', '$scope'];
 
-        // Note that the OfferAssistance and ReviewAssistance views are not nested
-        // under the BeaconDetails view, so we must use $rootScope instead of $scope.
-        $rootScope.selectionState = {
-          currentBeacon: undefined
-        };
-
-        cleanup = $scope.$watch('dataFromServer.beacons.length', function(newVal, oldVal) {
-          console.log('The number of beacons changed', newVal, oldVal);
-          $scope.selectionState.currentBeacon = $scope.findBeaconById($scope.$stateParams.id);
-        });
-
-        $scope.goToReviewAssistance = function() {
-          $scope.navigationService.navigateTo(
-            'dashboard.beacons.detail.review.response',
-            { responseId: $scope.selectionState.currentBeacon.responses[0].id });
-        };
-
-        $scope.$on('$destroy', function() {
-          cleanup();
-        });
-      }
-    ]
-  );
+require('./_module').controller('BeaconDetailsController', BeaconDetailsController);
